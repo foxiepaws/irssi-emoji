2cc1afcf82ee98266c07128f7cc8a91d14584192
Joshua Theze <fox@foxiepa.ws>
2014-04-21 21:14:50 -0400

    this actually works. %e(unamused) -> :unamused:

--- emoji.pl
+++ emoji.pl
@@ -3,7 +3,7 @@ use warnings;
 use utf8;
 
 use Irssi;
-
+use Data::Dumper;
 
 
 our $VERSION = '0.0.0';
@@ -11,25 +11,32 @@ our %IRSSI = (
     authors     => 'foxiepaws',
     contact     => 'fox@foxiepa.ws',
     name        => 'irssi-emoji',
-    description => 'adds tab completion for emoji in irssi.'
+    description => 'adds tab completion for emoji in irssi.',
     license     => 'BSD 2-Clause',
 );
 
 # list of emoji.
 our %emoji = (
-)
+    unamused => "\x{1F612}"
+);
 
 
 sub completer {
-
+    print Dumper @_;
     my ($strings, $window, $word, $linestart, $want_space) = @_;
 
     # we have two completion points, first is when the we match %e(: which
-    # should match emoji shortnames and %e(name): should complete to the 
+    # should match emoji shortnames and %e(name): should complete to the
     # emoji.
-    
+
     return unless ($word =~ /%e\(/);
 
+    # 
+    if ($word =~ /%e\((\w+)\)/) {
+       push @$strings, $emoji{$1};
+       $$want_space = 1; 
+       Irssi::signal_stop();
+    }
 }
 
 Irssi::signal_add_first('complete word', \&completer);
